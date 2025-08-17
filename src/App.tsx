cc7e0e262ad9a390740d658dd3faff26e43c1d01
Joseph Barbosa <josephbarbosa@Josephs-MacBook-Air.local>
2025-08-16 21:24:30 -0400

    initialized with vite ans styled header /  desc

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <main>
+      </main>
+    </>
+  )
+}
+
+export default App
