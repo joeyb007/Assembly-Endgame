f0aca6ff792ff54ba4ae8bb4ccebdeddd5943599
Joseph Barbosa <josephbarbosa416@gmail.com>
2025-08-21 20:48:25 -0400

    cleared adding keyboard functionality from backlog. added functionality for winning, losing, and farewell popups. disabled keyboard when game state changes to finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { languages } from './languages'
 import Chips from './components/chips'
 import './styles/App.css'
 import { clsx } from 'clsx'
+import { getFarewellText } from './utils'
 function App() {
   const [currentWord, setCurrentWord] = useState<string>('react')
   const alphapbet = "abcdefghijklmnopqrstuvwxyz"
@@ -12,6 +13,15 @@ function App() {
   const isGameLost = languages.length - 1 == wrongGuessesCount 
   const isGameWon = currentWord.split('').every(correctLetter => guessedLetters.includes(correctLetter))
   const isGameOver = isGameLost || isGameWon
+  const lastGuess = guessedLetters[guessedLetters.length - 1]
+  const isLastGuessWrong = !currentWord.includes(lastGuess)
+  const gameResultStyles : React.CSSProperties = {
+    visibility: wrongGuessesCount == 0 && !isGameOver ? "hidden" : "visible",
+    backgroundColor: isGameOver ? (isGameWon ? '#10A95B':'#BA2A2A') :  (isLastGuessWrong ? "#7A5EA7" : 'rgba(0,0,0,0)')
+  }
+
+
+
   const languageChips = languages.map((language, index) => {
     return <Chips key={language.name} 
                   name={language.name} 
@@ -31,21 +41,41 @@ function App() {
     const isIncorrect = isGuessed && !currentWord.includes(letter)
     const classes = clsx('keyboardButton', isCorrect && 'correctGuess', isIncorrect && 'incorrectGuess')
     return (
-    <button onClick={isGameOver ? undefined : () => handleKeyboardClick(letter)}
+    <button onClick={() => handleKeyboardClick(letter)}
+            disabled={isGameOver}
             key={letter} 
             className={classes}>
               {letter.toUpperCase()}
             </button>)
           })
+    // Adding a useEffect which only runs when the game starts and ends to add and remove event listeners
+    // on each keyboard key in the alphabet so that instead of clicking, users can also opt for physical keyboard
+  
+    useEffect(() => {
+      const handleKeyBoardClick = (e: KeyboardEvent) => {
+        const characterPressed = e.key.toLowerCase();
+        if (/^[a-z]$/.test(characterPressed) && !isGameOver) {
+          handleKeyboardClick(characterPressed);
+        }
+      }
+      document.addEventListener('keydown', handleKeyBoardClick)
+      return () => document.removeEventListener('keydown', handleKeyBoardClick)
+    },[isGameOver])
   return (
     <main>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
-      <section className='gameResult'>
-        <h2>You Win!</h2>
-        <h2>Well Done! 🎉</h2>
+      <section  style={gameResultStyles}
+                className='gameResult'>
+        {isGameOver ? (
+        <>
+          <h2>{isGameWon ? 'You Win!' : 'Game Over!'}</h2>
+          <h2>{isGameWon ? 'Well Done! 🎉' : 'You lose! Better start learning Assembly 😭'}</h2>
+        </>
+        ) : ((lastGuess && isLastGuessWrong) ? <h2>{getFarewellText(languages[wrongGuessesCount - 1].name)}</h2> : undefined)
+      }
       </section>
       <section className='chipsContainer'>
         {languageChips}
